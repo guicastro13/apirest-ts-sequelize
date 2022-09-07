fdc575468de06c66a3d14bc29e8b41a0a3c2c660
Guilherme Castro <guii_1@hotmail.com>
2022-09-07 10:18:44 -0300

    feat: criado rotas de update e delete

--- src/controllers/candidates-controller.ts
+++ src/controllers/candidates-controller.ts
@@ -4,43 +4,79 @@ import { Candidate } from "../models/candidate";
 export const candidatesController = {
   index: async (req: Request, res: Response) => {
     try {
-        const candidates = await Candidate.findAll();
-        return res.send(candidates);
-    } catch (err) {   
-        if ( err instanceof Error) {
-            return res.status(400).json({ message: err.message})
-        }
+      const candidates = await Candidate.findAll();
+      return res.send(candidates);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
     }
   },
 
-
   save: async (req: Request, res: Response) => {
-    const { name, email, bio, phone, openToWork} = req.body
+    const { name, email, bio, phone, openToWork } = req.body;
     try {
-        const candidate = await Candidate.create({
-            name,
-            email,
-            bio,
-            phone,
-            openToWork
-        })
-        return res.status(201).json(candidate)
+      const candidate = await Candidate.create({
+        name,
+        email,
+        bio,
+        phone,
+        openToWork,
+      });
+      return res.status(201).json(candidate);
     } catch (err) {
-        if (err instanceof Error) {
-            return res.status(400).json({ message: err.message})
-        }
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
     }
   },
 
   show: async (req: Request, res: Response) => {
-    const { id } = req.params
+    const { id } = req.params;
     try {
-        const candidate = await Candidate.findByPk(id)
-        res.status(201).json(candidate)
+      const candidate = await Candidate.findByPk(id);
+
+      res.status(201).json(candidate);
     } catch (err) {
-        if (err instanceof Error) {
-            res.status(400).json({ message: err.message })
-        }
+      if (err instanceof Error) {
+        res.status(400).json({ message: err.message });
+      }
     }
-  }
+  },
+
+  update: async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, email, bio, phone, openToWork } = req.body;
+    try {
+      const candidate = await Candidate.findByPk(id);
+      if (candidate == null) {
+        return res.status(404).json({ message: "didn't find this candidate." });
+      }
+      candidate.name = name;
+      candidate.email = email;
+      candidate.bio = bio;
+      candidate.phone = phone;
+      candidate.openToWork = openToWork;
+
+      await candidate.save();
+
+      return res.status(200).json(candidate);
+    } catch (err) {
+      if (err instanceof Error) {
+        res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
+  delete: async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      await Candidate.destroy({ where: { id } });
+      return res.status(204).send();
+    } catch (err) {
+      if (err instanceof Error) {
+        res.status(400).json({ message: err.message });
+      }
+    }
+  },
 };
--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,11 @@ import { candidatesController }from './controllers/candidates-controller';
 
 const router = express.Router()
 
-router.get('/', (req, res) => res.json({hello: " world"})) 
 router.get('/candidates', candidatesController.index)
+router.post('/candidates', candidatesController.save)
 router.get('/candidates/:id', candidatesController.show)
+router.post('/candidates/:id', candidatesController.update)
+router.delete('/candidates/:id', candidatesController.delete)
 
 
 export { router }
